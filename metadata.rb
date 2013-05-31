3e70e5e1d24b5a3da72477cc2c02628ec7e1fe71
Bryan Taylor <btaylor@rackspace.com>
2013-05-31 14:03:15 -0500

    add iptables cookbook as dependency

--- metadata.rb
+++ metadata.rb
@@ -12,7 +12,7 @@ recipe "nginx::source", "Installs nginx from source and sets up configuration wi
   supports os
 end
 
-%w{ build-essential yum apt runit }.each do |cb|
+%w{ build-essential yum apt runit iptables }.each do |cb|
   depends cb
 end
 
